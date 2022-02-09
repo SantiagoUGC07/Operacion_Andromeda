6172abde9cbf927087f3cfc06a62933293538591
Eduardo Diaz <eduardo9dza@gmail.com>
2022-02-08 20:42:59 -0600

    Lalo

---
+++ public/js/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel-games').slick({
+    slidesToShow: 5,
+    dots:true,
+    centerMode: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+    });
+  });
