856e0450cf289463db6dbdf96d4e1622851f9209
Daniel Nava <danielnavagarcia1@gmail.com>
2022-02-16 16:36:15 -0600

    foto de jose

--- public/js/team.js
+++ public/js/team.js
@@ -20,17 +20,23 @@ jQuery('.parte-de-abajo').click(function () {
     $('#miembro' + $(this).attr('target')).show();
 });
 
-/* tiempo(function(){
+/* tiempo(function () {
     cambiarMiembro();
 }, 4000);
 
-function cambiarMiembro () {
-    //pone en blanco y negro la carta que ya no esta
-    $('.imagen-selector').addClass("blanco-y-negro");
-    //quita todo lo que esta arriba
-    $('.parte-de-arriba').hide();
-    //pone color a la imagen de abajo
-    $('#selector' + $(this).attr('target')).removeClass("blanco-y-negro");
-    //muestra arriba la imagen que se selecciono abajo
-    $('#miembro' + $(this).attr('target')).show();
-}; */
+function cambiarMiembro() {
+    let auto = 6;
+
+    $('#miembro' + auto).show();
+    $('#selector' + auto).removeClass("blanco-y-negro"); */
+
+
+    /*     //pone en blanco y negro la carta que ya no esta
+        $('.imagen-selector').addClass("blanco-y-negro");
+        //quita todo lo que esta arriba
+        $('.parte-de-arriba').hide();
+        //pone color a la imagen de abajo
+        $('#selector' + $(this).attr('target')).removeClass("blanco-y-negro");
+        //muestra arriba la imagen que se selecciono abajo
+        $('#miembro' + $(this).attr('target')).show(); */
+/* };  */
