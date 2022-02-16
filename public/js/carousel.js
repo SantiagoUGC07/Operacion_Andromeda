97780bf6706818235bd81526a734835120827d80
Daniel Nava <danielnavagarcia1@gmail.com>
2022-02-16 05:51:29 -0600

    plantilla

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -5,6 +5,25 @@ $(document).ready(function(){
     centerMode: true,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 3000
     });
+
+    
+  });
+
+  $('.banners-carousel').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 1,
+  });
+
+  $('.productos-carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    centerMode: true,
+    autoplaySpeed: 2500
   });
