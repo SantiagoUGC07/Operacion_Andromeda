0a5e5bea9b426b6e94f5820838aae696b2bd8372
luis-ao <96146916+luis-ao@users.noreply.github.com>
2022-02-18 16:27:21 -0600

    se crea presets para preloader

---
+++ public/js/preloader.js
@@ -0,0 +1,7 @@
+var loader = document.getElementById("loader");
+window.addEventListener("load", function () {
+    loader.style.height = "100%";
+    loader.style.width = "100%";
+    loader.style.borderRadius = "50%";
+    loader.style.visibility = "hidden"
+});
